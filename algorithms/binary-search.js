60c982d4029d2be11ae82f42a46f0a713adac0ff
zakharovda95 <zakharovda95@gmail.com>
2022-08-10 17:00:43 +0300

    binary search

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -1,28 +1,37 @@
 /** Алгоритм: Бинарный поиск**/
 
-const ARRAY= []
+const ARRAY = []
 for (let i = 1; i <= 1000; i++) {
     ARRAY.push(i);
 }
 
-const value = 863;
+const value = 862;
 
 function binarySearch(array, value) {
+    let count = 0;
     let min = array[0];
-    let max = array[array.length - 1];
-     while (min <= max) {
-        const middle = (min + max) / 2;
+    let max = array.at(-1);
+    while (min <= max) {
+        const middle = Math.floor((min + max) / 2);
         let result = ARRAY[middle]
-         if (result === value) {
-             result = value
-         }
-         if (result < value) {
-             min = middle + 1
-         }
-         if (result > value) {
-             max = middle - 1
-         }
-     }
+        if (result === value) {
+            count++
+            return {res: middle, count}
+        }
+        if (result < value) {
+            count++
+            min = middle - 1
+        }
+        if (result > value) {
+            count++
+            max = middle + 1
+        }
+    }
+    return 'Not found!'
 }
 
-binarySearch(ARRAY, value)
+
+const result = binarySearch(ARRAY, value);
+
+const {res, count} = result
+console.log(res, count);
